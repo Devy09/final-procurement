cf6e9ba24b529dc0bf519df010775a22c2b7f1d4
Devy09 <dgeregali@gmail.com>
2025-03-06 00:16:22 +0800

    Please work

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: undefined, // Remove this line if not needed
+  trailingSlash: true, // Ensures correct file paths
+  images: {
+    unoptimized: true, // Fix issues with Next.js image optimization on Vercel
+  }
 };
 
 module.exports = nextConfig;
